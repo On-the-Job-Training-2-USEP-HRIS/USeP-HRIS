dff1019f8b6120d36a3ed21ff2dc33254519020b
rshielajoy <regisshielajoy@gmail.com>
2019-09-19 17:45:57 +0800

    removed google api font

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,7 +10,6 @@
 	<link href="{{ asset('libraries/custom-css/layout.css') }}" rel="stylesheet">
 	
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-	<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 </head>
 
 <body>
