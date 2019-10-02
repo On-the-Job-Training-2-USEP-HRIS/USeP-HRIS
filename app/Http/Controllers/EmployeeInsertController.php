9c9e6dd81890ecae8fb39587fbcf9abc27d60159
RahimAJ <rahimjumlani@gmail.com>
2019-10-02 11:04:49 +0800

    Fixed bug in user creation in line with inserting data, will now correspond to newly created user (MAXId)

--- app/Http/Controllers/EmployeeInsertController.php
+++ app/Http/Controllers/EmployeeInsertController.php
@@ -11,8 +11,9 @@ class EmployeeInsertController extends Controller
         $formdataresult = json_decode(json_encode($formcontent),true);
         // dd($formcontent);
 
-        foreach($formdataresult as $key => $datavalue){
+        $user_created = false;
 
+        foreach($formdataresult as $key => $datavalue){
             // User generation
             if($key == "1"){
                 $username_first = $datavalue[2]; // First half of username generation : Surname
@@ -21,39 +22,47 @@ class EmployeeInsertController extends Controller
                 $username_last = $datavalue[2]; // Second half of username generation
                 $last_userid = \DB::select('call getPDS_userID'); // Select the latest employee ID to be added to username : First Name
                 $last_userid_result = $last_userid[0] -> maxID;
+                if ($last_userid_result == NULL){ // Handles the first user creation
+                    $last_userid_result = 0;
+                }
                 $user_comb = strtolower(str_replace(' ', '', $username_first . "." . $username_last)) . "." . ($last_userid_result + 1); // Username combination of first and second half with the latest employee ID
                 $default_pass = password_hash("emp@123", PASSWORD_DEFAULT);
-                // dd($user_comb);
-
-                \DB::statement("CALL insert_PDS_user('$user_comb', '$default_pass')");
+                
+                
+                \DB::statement("CALL insert_PDS_user('$user_comb', '$default_pass')"); // Store to PDS_user table
+                $user_created = true;
             // End of User generation
-
             }
+        }
 
-            if($key > 0){
-                // echo $datavalue[0] . "<br>"; Group ID
-                // echo $datavalue[1] . "<br>"; Fieldsubfied ID
-                // echo $datavalue[2] . "<br>"; Data
-                if ( ! isset($datavalue[2])) {
-                    if ($datavalue[0] == 4){
-                        // $datavalue[2] = date('Y-m-d H:i:s'); // Datetime problem if Null
-                        $date_default = '00-00-0000 00:00:00'; // 1970-01-01 00:00:00 will be input in the database
-                        $datavalue[2] = date ("Y-m-d H:i:s", strtotime($date_default));
-                    } else if ($datavalue[0] == 1){
-                        $datavalue[2] = "0";    // Digit does not accept Null
-                    } else {
-                        $datavalue[2] = null;
+        if($user_created == true){
+            foreach($formdataresult as $key => $datavalue){
+                if($key > 0){
+                    // echo $datavalue[0] . "<br>"; Group ID
+                    // echo $datavalue[1] . "<br>"; Fieldsubfied ID
+                    // echo $datavalue[2] . "<br>"; Data
+                    if ( ! isset($datavalue[2])) {
+                        if ($datavalue[0] == 4){
+                            // $datavalue[2] = date('Y-m-d H:i:s'); // Datetime problem if Null
+                            $date_default = '00-00-0000 00:00:00'; // 1970-01-01 00:00:00 will be input in the database
+                            $datavalue[2] = date ("Y-m-d H:i:s", strtotime($date_default));
+                        } else if ($datavalue[0] == 1){
+                            $datavalue[2] = "0";    // Digit does not accept Null
+                        } else {
+                            $datavalue[2] = null;
+                        }
+                     }
+    
+                     // Filtering of data to be inserted into database table according to data group
+                    //  dd($datavalue[1]);
+                     
+                    \DB::statement("CALL insert_userdata('$datavalue[1]')");
+                    switch ($datavalue[0]) {
+                        case 1 : \DB::statement("CALL insert_datadigit('$datavalue[2]')"); break;
+                        case 2 : \DB::statement("CALL insert_datatext('$datavalue[2]')"); break;
+                        case 3 : \DB::statement("CALL insert_datafile('$datavalue[2]')"); break;
+                        case 4 : \DB::statement("CALL insert_datadate('$datavalue[2]')"); break;
                     }
-                 }
-
-                 // Filtering of data to be inserted into database table according to data group
-                 
-                \DB::statement("CALL insert_userdata('$datavalue[1]')");
-                switch ($datavalue[0]) {
-                    case 1 : \DB::statement("CALL insert_datadigit('$datavalue[2]')"); break;
-                    case 2 : \DB::statement("CALL insert_datatext('$datavalue[2]')"); break;
-                    case 3 : \DB::statement("CALL insert_datafile('$datavalue[2]')"); break;
-                    case 4 : \DB::statement("CALL insert_datadate('$datavalue[2]')"); break;
                 }
             }
         }
